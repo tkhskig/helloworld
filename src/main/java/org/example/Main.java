c32a1515776c7be39480691bd2b932f1bababb21
tkhskig <tk5.4497@outlook.com>
2023-11-25 00:01:33 +0900

    試運転

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            if (i < 10) {
+                System.out.println(i + " : helloworld");
+            } else {
+                System.out.println(i + " : hello");
+            }
+        }
+    }
+}
